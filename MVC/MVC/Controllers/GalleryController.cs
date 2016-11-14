ff07fa3cf1009835d135178c6f9202f9c1350be8
mrBosse <bobjjansson@gmail.com>
2016-11-14 15:13:22 +0100

    added controller

---
+++ MVC/MVC/Controllers/GalleryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Gallery()
+        {
+            List<Photo> photos = new List<Photo>();
+
+            photos.Add(new Photo { PhotoName = "1McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "2McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "3McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "4McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "5McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "6McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "7McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "8McGregor.jpg" });
+            photos.Add(new Photo { PhotoName = "9McGregor.jpg" });
+
+
+            return View(photos);
+        }
+    }
+}
---
+++ MVC/MVC/Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Model;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string PhotoName { get; set; }
+    }
+}
